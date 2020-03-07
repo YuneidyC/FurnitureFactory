fed2b5d2ecc2161b5ac870e200101012f6f7a1bb
YuneidyC <yuneidycastillo25@gmail.com>
2020-03-07 20:52:19 +0100

    Assign order to craftsman by boss

--- src/people/Boss.java
+++ src/people/Boss.java
@@ -1,9 +1,32 @@
 package people;
 
+import java.util.List;
+import java.util.Random;
+
+import factory.Order;
+
 public class Boss extends Employee {
 
 	public Boss(String id, String name) {
 		super(id, name);
 	}
 
+	public boolean assignCraftsman(Order order, List<Craftsman> artisanList) {
+		if (order == null || artisanList == null || artisanList.isEmpty()) {
+			return false;
+		}
+
+		if (order.getEmployeeAssigned() != null) {
+			return false;
+		}
+
+		// We don't want to always assign orders to the same artisan.
+		int sizeCraftsmanList = artisanList.size();
+		Random randomNumbers = new Random();
+		int randomIndex = randomNumbers.nextInt(sizeCraftsmanList);
+		Craftsman craftsman = artisanList.get(randomIndex);
+		order.setEmployeeAssigned(craftsman.getId());
+		craftsman.assignOrder(order);
+		return true;
+	}
 }
--- src/people/Craftsman.java
+++ src/people/Craftsman.java
@@ -1,9 +1,23 @@
 package people;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import factory.Order;
+
 public class Craftsman extends Employee {
+
+	private List<Order> assignedOrders = new LinkedList<Order>();
 
 	public Craftsman(String id, String name) {
 		super(id, name);
 	}
 
+	public List<Order> getAssignedOrders() {
+		return assignedOrders;
+	}
+
+	public void assignOrder(Order order) {
+		assignedOrders.add(order);
+	}
 }
