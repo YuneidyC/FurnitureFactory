cf47b01b5beb80419b26d4f9e2e078c7c7415cf1
YuneidyC <yuneidycastillo25@gmail.com>
2020-03-08 01:01:49 +0100

    First implementation

--- src/factory/Factory.java
+++ src/factory/Factory.java
@@ -2,17 +2,50 @@ package factory;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 import people.Person;
 
 public class Factory {
 
+	private static void mainFunction() {
+		System.out.println("Artisan Factory");
+		System.out.println("1.People");
+		System.out.println("2.Furniture");
+		System.out.println("3.Orders");
+		System.out.println("4.Exit");
+		System.out.println("Enter the number: ");
+	}
+
 	private List<Person> people = new LinkedList<Person>();
 	private List<Order> orders = new LinkedList<Order>();
 
 	public static void main(String[] args) {
-		Factory factory = new Factory();
-		Test.fillEmployeesList(factory.people);
-		Test.createOrder(factory.orders);
+
+		boolean mustExit = false;
+		while (!mustExit) {
+			Factory.mainFunction();
+			Scanner sc = new Scanner(System.in);
+			String str = sc.nextLine();
+			int integer = Integer.parseInt(str);
+			try {
+				switch (integer) {
+				case 0:
+					System.out.println("People");
+					break;
+				case 1:
+					System.out.println("Furniture");
+					break;
+				case 4:
+					mustExit = true;
+					break;
+				default:
+					break;
+				}
+			} catch (NumberFormatException exception) {
+				System.out.println("this is not a number");
+			}
+
+		}
 	}
 }
